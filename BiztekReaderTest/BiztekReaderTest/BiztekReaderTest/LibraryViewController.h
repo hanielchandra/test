67b876edaf358bd6ff23ae116254b77d1afc5612
Haniel Chandra <Haniel_chandra@hotmail.com>
2012-07-19 21:59:06 +0700

    update terakhir hari ini

--- BiztekReaderTest/BiztekReaderTest/BiztekReaderTest/LibraryViewController.h
+++ BiztekReaderTest/BiztekReaderTest/BiztekReaderTest/LibraryViewController.h
@@ -2,11 +2,21 @@
 
 @interface LibraryViewController : UIViewController
 {
-
+    UIScrollView* LibraryScrollView1, LibraryScrollView2;
+    UIButton* buttonfilter1, buttonfilter2;
+    UITableView* tableview1, tableview2;
+    UITableViewCell* tableviewcell1,tableviewcell2;
 }
 
+
+@property (retain, nonatomic) IBOutlet UIScrollView* LibraryScrollView1,LibraryScrollView2; 
 @property (retain, nonatomic) IBOutlet UIButton* buttonfilter1, buttonfilter2;
 @property (retain, nonatomic) IBOUtlet UITableView* tableview1, tableview2;
 @property (retain, nonatomic) IBOutlet UITableViewCell* tableviewcell1,tableviewcell2;
+
+-(void)scrollViewDidScroll: (UIScrollView *)sender;
+-(void)scrollViewWillBeginDragging: (UIScrollView *) sender;
+- (IBAction)changePage:(id)sender;
+
 
 @end
