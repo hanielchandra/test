2f300c2466c3aa69743b92fc613925e8ce9af7b9
Haniel Chandra <Haniel_chandra@hotmail.com>
2012-07-19 15:32:50 +0700

    ok mari kita syncing lagi

--- BiztekReaderTest/BiztekReaderTest/BiztekReaderTest/LibraryViewController.h
+++ BiztekReaderTest/BiztekReaderTest/BiztekReaderTest/LibraryViewController.h
@@ -5,6 +5,8 @@
 
 }
 
-@property
+@property (retain, nonatomic) IBOutlet UIButton* buttonfilter1, buttonfilter2;
+@property (retain, nonatomic) IBOUtlet UITableView* tableview1, tableview2;
+@property (retain, nonatomic) IBOutlet UITableViewCell* tableviewcell1,tableviewcell2;
 
 @end
